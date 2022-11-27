08b004c6dba30b91079faf942529caba3b6d540f
lopo <lopo@zju.edu.cn>
2022-11-27 23:56:21 +0800

    feat: toast

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,22 @@ import { Outlet } from "react-router-dom";
 import { RecoilRoot } from "recoil"
 import Splash from "@/views/Splash";
 import BannerMenu from "@/layouts/BannerMenu";
+import { ToastContainer } from "react-toastify";
 
 export default function App() {
     return (
         <RecoilRoot>
-            <div className="w-full h-full rounded-lg border border-t-0 border-b30 bg-b10 flex flex-col items-center justify-between overflow-hidden">
+            <div
+                className="w-full h-full rounded-lg border border-t-0 border-b30 bg-b10 flex flex-col items-center justify-between overflow-hidden">
                 <BannerMenu/>
 
-                <div className="w-full flex-1">
+                <div className="w-full my-4 flex-1 flex-shrink-0 overflow-x-hidden overflow-y-scroll">
                     <Suspense fallback={ <Splash/> }>
                         <Outlet/>
                     </Suspense>
                 </div>
+
+                <ToastContainer/>
             </div>
         </RecoilRoot>
     )
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import "tailwindcss/tailwind.css"
+import "react-toastify/dist/ReactToastify.css"
 import "@fontsource/roboto/300.css"
 import "@fontsource/roboto/400.css"
 import "@fontsource/roboto/500.css"
